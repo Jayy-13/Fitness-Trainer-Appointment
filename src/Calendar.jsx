03f3f33ac729c29de4d1ea280e7f4f11d6d5b6c1
Jayy-13 <97361502+Jayy-13@users.noreply.github.com>
2023-12-11 02:54:47 +0530

    Update Calendar.jsx

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -1,26 +1,44 @@
-import React from 'react';
-// import { Calendar, momentLocalizer } from 'react-big-calendar';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
+import React, { useState } from "react";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
-// BigCalendar.setLocalizermomentLocalizer(moment));
 
-import { Calendar, momentLocalizer } from 'react-big-calendar'
-import moment from 'moment'
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
 
-const localizer = momentLocalizer(moment)
+const localizer = momentLocalizer(moment);
 
-export default function CalendarComp({clients}){
-    return (
-        <div style={{ height: '100vw', position: 'relative' }} className='calendar'>
-            <button >Back to Clients Page</button>
-          <Calendar
-            localizer={localizer}
-            events={clients.map(client => ({
-              title: `${client.firstName} ${client.lastName}`,
-              start: new Date(client.appointments.date + ' ' + client.appointments.time),
-              end: new Date(client.appointments.date + ' ' + client.appointments.time),
-            }))}
-          />
-        </div>
+export default function CalendarComp({ clients, backToClients }) {
+  const [date, setDate] = useState(new Date());
+  const [view, setView] = useState("month");
+  // const events = clients.map((client) => ({
+  //   title: `${client.firstName} ${client.lastName}`,
+  //   start: new Date(client.appointments.date + " " + client.appointments.time),
+  //   end: new Date(client.appointments.date + " " + client.appointments.time),
+  // }));
+
+  const events = clients.flatMap((client) =>
+    client.appointments.map((appointment) => {
+      const startDateTime = new Date(
+        `${appointment.date} ${moment(appointment.time, "HH:mm").format("HH:mm")}`
       );
+      const endDateTime = new Date(startDateTime);
+    endDateTime.setHours(startDateTime.getHours() + 1);
+      
+      return {
+        title: `${client.firstName} ${client.lastName}`,
+      start: startDateTime,
+      end: endDateTime,
+      }
+    })
+  )
+
+  return (
+    <div style={{ height: "500px" }} className="calendar">
+      <button onClick={backToClients}>Back to Clients Page</button>
+      <Calendar
+        localizer={localizer}
+        events={events}
+      />
+    </div>
+  );
 }

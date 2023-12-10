58ad6da41981ca27fe1f1f46ad0f925930c40c6f
Jayy-13 <97361502+Jayy-13@users.noreply.github.com>
2023-12-11 03:00:07 +0530

    Update AppointmentForm.jsx

--- src/AppointmentForm.jsx
+++ src/AppointmentForm.jsx
@@ -23,23 +23,43 @@ export default function AppointmentForm({client, onSubmit, onCancel}) {
   };
 
   return (
-    <div>
-      <label>Date: </label>
-      <input
-        type="date"
-        name="date"
-        value={formData.date}
-        onChange={handleChange}
-      />
-      <label>Time: </label>
-      <input
-        type="time"
-        name="time"
-        value={formData.time}
-        onChange={handleChange}
-      />
-      <button onClick={handleSubmit}>Add Appointment</button>
-      <button onClick={onCancel}>Cancel</button>
+    <div
+      className="modal show"
+      style={{ display: "block", position: "fixed", margin: "7rem 0.5rem" }}
+    >
+      <Modal.Dialog>
+        <Modal.Header>
+          <Modal.Title>Add Appointment Form</Modal.Title>
+        </Modal.Header>
+
+        <Modal.Body>
+          <label htmlFor="">Date : </label>
+          <input
+            type="date"
+            name="date"
+            value={formData.date}
+            onChange={handleChange}
+          />
+          <br />
+          <br />
+          <label htmlFor="">Time : </label>
+          <input
+            type="time"
+            name="time"
+            value={formData.time}
+            onChange={handleChange}
+          />
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="secondary" onClick={onCancel}>
+            Close
+          </Button>
+          <Button variant="light" onClick={handleSubmit}>
+            Add Client
+          </Button>
+        </Modal.Footer>
+      </Modal.Dialog>
     </div>
   );
 }

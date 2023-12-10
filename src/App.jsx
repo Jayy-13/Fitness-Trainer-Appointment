6b02fbfe501b21bd42d7d283f98b1afbdad7998e
Jayy-13 <97361502+Jayy-13@users.noreply.github.com>
2023-12-11 02:58:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,39 +12,7 @@ function App() {
       </header>
       <main className="main">
         <section>
-          {/* <div className="tableWrapper">
-            <table>
-              <thead id="thead">
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Location</th>
-                <th>Appointments</th>
-
-              </thead>
-              <tbody id="tablebody">
-                <tr className="wholeRow">
-                  <td>Jayanth</td>
-                  <td>B</td>
-                  <td>Bangalore</td>
-                  <td>
-                    <div className="appointment">
-                      <ul>
-                        <li>
-                          10/12/2023 - 10:00 AM <button>Edit</button>{" "}
-                          <button>Delete</button>
-                        </li>
-                        <li>
-                          20/12/2023 - 11:00 AM <button>Edit</button>{" "}
-                          <button>Delete</button>
-                        </li>
-                      </ul>
-                      <button>Add Appointment</button>
-                    </div>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div> */}
+          
           <ClientList />
         </section>
       </main>
